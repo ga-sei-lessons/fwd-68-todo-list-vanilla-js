e2cb88a078b710bbda6f737d93dc284ec299a66b
weston-bailey <wdbailey87@gmail.com>
2021-07-29 15:50:24 -0700

    one event listener to rule them all

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
-// 1. ADDING TO-DOS
-
-
 /* APP STATE */
-
-// Our array of to-dos - we will push a to-do to this array when a new to do is added, and then render the whole list of to-dos as `li` elements in our `ul#to-do-list` element
 let toDos = localStorage.getItem("toDos") 
 let completedToDos = localStorage.getItem("completedTodos") 
 
@@ -23,42 +18,49 @@ if (completedToDos) {
 
 /* DOM ELEMENTS */
 
-const completedToDoList = document.querySelector("#completed-list");
-const toDoList = document.querySelector("#to-do-list");
-const form = document.querySelector("#to-do-form");
-const toDoInput = document.querySelector("#to-do-input");
+const completedToDoList = document.querySelector("#completed-list") 
+const toDoList = document.querySelector("#to-do-list") 
+const form = document.querySelector("#to-do-form") 
+const toDoInput = document.querySelector("#to-do-input") 
 const toDoCount = document.querySelector("#to-do-count")
 const completedToDoCount = document.querySelector("#completed-to-do-count")
 
 /* EVENT LISTENERS */
 
-form.addEventListener("submit", addToDo);
+form.addEventListener("submit", addToDo) 
+// one event listener to rule them all
+document.addEventListener("click", event => {
+  if (event.target.matches(".to-do-item")) {
+    const toDo = event.target.innerText 
+    completeToDo(toDo) 
+  }
+}) 
 
 /* FUNCTIONS */
 
 // adds a todo to our list and rerenders the todos
 function addToDo(event) {
   // Prevents default HTML action on a form submit - page does not refresh
-  event.preventDefault();
+  event.preventDefault() 
   // Check if user has typed anything in input element
-  const toDo = toDoInput.value;
+  const toDo = toDoInput.value 
   if (toDo) {
     // Add typed string to `toDos` array
-    toDos.push(toDo);
+    toDos.push(toDo) 
     // store todo in local storate
-    window.localStorage.setItem("toDos", JSON.stringify(toDos));
+    window.localStorage.setItem("toDos", JSON.stringify(toDos)) 
     // Render all to-dos
-    renderToDos();
+    renderToDos() 
   }
   
   // Clear value from input element - input is now blank again
-  toDoInput.value = "";
+  toDoInput.value = "" 
 }
 
 // clears the list of todos and rerenders
 function renderToDos() {
   // Clear `ul` element of HTML before rendering - prevents double rendering of to-dos
-  // toDoList.innerHTML = ""; // bad way of doing things
+  // toDoList.innerHTML = ""  // bad way of doing things
 
   // do it like this 
   while(toDoList.firstChild) {
@@ -66,30 +68,30 @@ function renderToDos() {
   }
 
   // For each element in array, render that toDo
-  for (let i = 0; i < toDos.length; i++) {
-    renderToDo(toDos[i]);
+  for (let i = 0;  i < toDos.length;  i++) {
+    renderToDo(toDos[i]) 
   }
   // Can also use for...of loop
   // for (toDo of toDos) {
-  //   renderToDo(toDo);
+  //   renderToDo(toDo) 
   // }
 
-  toDoCount.innerText = toDos.length;
+  toDoCount.innerText = toDos.length 
 }
 
 // adds one todo to DOM and with a click event on it
 function renderToDo(toDo) {
   // create element
-  const li = document.createElement("li");
+  const li = document.createElement("li") 
   // add css class
-  li.classList.add("to-do-item");
+  li.classList.add("to-do-item") 
   // update the text
-  li.innerText = toDo;
+  li.innerText = toDo 
   // append to the DOM
-  toDoList.appendChild(li);
+  toDoList.appendChild(li) 
   // add event listen to complete todo
   li.addEventListener("click", function() {
-    completeToDo(toDo);
+    completeToDo(toDo) 
   })
 }
 
@@ -98,39 +100,39 @@ function renderToDo(toDo) {
 // When user clicks a to-do `li` element, this function is called
 function completeToDo(toDo) {
   // add completed to-do to completed array
-  completedToDos.push(toDo);
+  completedToDos.push(toDo) 
   
   // Remove completed to-do from to-dos array
   // https://stackoverflow.com/questions/5767325/how-can-i-remove-a-specific-item-from-an-array
-  toDos = toDos.filter(t => t !== toDo);
+  toDos = toDos.filter(t => t !== toDo) 
 
   // add completed todos to local storage
-  window.localStorage.setItem("toDos", JSON.stringify(toDos));
-  window.localStorage.setItem("completedTodos", JSON.stringify(completedToDos));
+  window.localStorage.setItem("toDos", JSON.stringify(toDos)) 
+  window.localStorage.setItem("completedTodos", JSON.stringify(completedToDos)) 
   // Re-render to-dos and completed to-dos
-  renderToDos();
-  renderCompletedToDos();
+  renderToDos() 
+  renderCompletedToDos() 
 }
 
 // Similar logic with rendering to-dos - but no click event listener
 function renderCompletedToDos() {
-  // completedToDoList.innerHTML = "";
+  // completedToDoList.innerHTML = "" // no! bad!
   while (completedToDoList.firstChild) {
     completedToDoList.removeChild(completedToDoList.firstChild)
   }
   
   for (completedToDo of completedToDos) {
-    renderCompleted(completedToDo);
+    renderCompleted(completedToDo) 
   }
 
-  completedToDoCount.innerText = completedToDos.length;
+  completedToDoCount.innerText = completedToDos.length 
 }
 
 function renderCompleted(completed) {
-  const li = document.createElement("li");
-  li.classList.add("done-item");
-  li.innerText = completed;
-  completedToDoList.appendChild(li);
+  const li = document.createElement("li") 
+  li.classList.add("done-item") 
+  li.innerText = completed 
+  completedToDoList.appendChild(li) 
 }
 
 // render one time on page load incase something was in local storage
