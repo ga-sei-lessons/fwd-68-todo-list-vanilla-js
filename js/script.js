75794921baf41e319d4137c7eeab12ffab46c2eb
weston-bailey <wdbailey87@gmail.com>
2021-07-29 14:50:26 -0700

    starter code

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let toDos = [];
+
+// 1. ADDING TO-DOS
+
+// When the user submits the #to-do-form form:
+// - Check if the user has typed something in the input
+//   - If yes, add the input value to the to-dos array above
+//   - Render the new to-do in the #to-do-list ul
+//     - Check the HTML for example to-do list item (commented out)
+//   - Update the to-do count next to "My To-Dos" whenever a to-do is added
+
+// 2. COMPLETE TO-DOS
+
+// When the user clicks a to-do list item
+// - Remove that to-do from the to-do array and list
+// - Add that to-do to a completed to-dos array and render that completed list 
+//     - Check the HTML for example completed to-do list item (commented out)
+// - Update the to-do count and completed to-do count in the headings
+
+// When should we attach the event listener for clicking a to-do list item?
